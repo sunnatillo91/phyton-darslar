96caf34216f3c8d10239bafa2a310f4299fd1384
sunnatillo91 <sunnatillo19912006@gmail.com>
2022-07-11 22:39:12 +0500

    New lessons added

---
+++ 35test/Practice_test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 11 12:05:24 2022
+
+#35-DARS. DASTURNI TEKSHIRISH  practice test
+
+Author: Sunnatillo Xayrullayev
+"""
+
+import unittest
+from Practice import sonKattami
+
+class sonKattamiTest(unittest.TestCase):
+    def test_true(self):
+        self.assertGreater(sonKattami(7, 5, 3)
+        
+unittest.main()
+
--- 35test/circle.py
+++ 35test/circle.py
@@ -15,5 +15,5 @@ def getArea(r, pi=3.14159):
     """Doiraning yuzini qaytaruvchi funksiya"""
     return pi*(r**2)
 
-def getPerimetr(r, pi=3.14159):
+def getPerimeter(r, pi=3.14159):
     return 2*pi*r
